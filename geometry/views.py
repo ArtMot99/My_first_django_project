b782fd1ed220a668cfe0e273df5ab728e8b901c7
Artem <104223700+ArtMot99@users.noreply.github.com>
2022-06-12 19:12:06 +0200

    Добавлен config для geometry, и несколько представлений для роутов, которые расчитывают площади треугольника, квадрата, круга.

--- geometry/views.py
+++ geometry/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def get_rectangle_area(request, width: int, height: int):
+    result = width * height
+    return HttpResponse(f'Площадь прямоугольника размером {width}x{height} равна {result}')
+
+
+def get_square_area(request, width: int):
+    result = width * width
+    return HttpResponse(f'Площадь квадрата размером {width}x{width} равна {result}')
+
+
+def get_circle_area(request, radius: int):
+    result = 3.14 * (radius**2)
+    return HttpResponse(f'Площадь круга радиуса {radius} равна {result}')
