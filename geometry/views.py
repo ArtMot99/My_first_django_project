379a794ef61ffc685b881f87bd9fd8da452f9fa2
Artem <104223700+ArtMot99@users.noreply.github.com>
2022-06-14 20:22:23 +0200

    Для geometry_app добавлено 3 роута + 3 представления, реализован redirect.

--- geometry/views.py
+++ geometry/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
 from django.shortcuts import render
 
 
@@ -7,11 +7,23 @@ def get_rectangle_area(request, width: int, height: int):
     return HttpResponse(f'Площадь прямоугольника размером {width}x{height} равна {result}')
 
 
+def redirect_get_rectangle_area(request, width: int, height: int):
+    return HttpResponseRedirect(f'/calculate_geometry/rectangle/{width}/{height}')
+
+
 def get_square_area(request, width: int):
     result = width * width
     return HttpResponse(f'Площадь квадрата размером {width}x{width} равна {result}')
 
 
+def redirect_get_square_area(request, width: int):
+    return HttpResponseRedirect(f'/calculate_geometry/square/{width}')
+
+
 def get_circle_area(request, radius: int):
     result = 3.14 * (radius**2)
     return HttpResponse(f'Площадь круга радиуса {radius} равна {result}')
+
+
+def redirect_get_circle_area(request, radius: int):
+    return HttpResponseRedirect(f'/calculate_geometry/circle/{radius}')
